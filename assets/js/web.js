fb0fa2803c713b546f5af8948c3b714fbac5a7e0
RickyRechtackova <erikarech.01@gmail.com>
2025-11-25 20:53:53 +0100

    mobile menu reorganisation

--- assets/js/web.js
+++ assets/js/web.js
@@ -1,15 +1,8 @@
 function openMenu() {
     let menu = document.getElementById("mylinks");
-    let icons = document.getElementById("sm-icons");
     if (menu.style.display === "block") {
         menu.style.display = "none";
     } else {
         menu.style.display = "block";
-    }
-
-    if (icons.style.display === "block") {
-        icons.style.display = "none";
-    } else {
-        icons.style.display = "block";
     }
 }
